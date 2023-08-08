3de031a4128430fa66546d0e25a9be576032397a
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-08-08 14:11:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 //you
 
 
-const text = prompt("Enter a string");
-alert(firstChar(text));
-
-const firstChar =(s)=>{
-	for(let i=0; i<s.length; i++){
-		 if(!s.contains(s.charAt(i)))
-			 return s.charAt(i);
-	}
-	return -1;
+function firstChar(s) {
+  let rev = s.split("").reverse().join("")
+  for(let i=0; i<s.length; i++){
+    if(rev.indexOf(s.charAt(i))+1-s.length===i)
+      return s.charAt(i)
+  }
 }
+
+// Do not change the code below
+
+const text = prompt("Enter text:");
+alert(firstChar(text));
