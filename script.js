a1d18eae6c2ebbceb208971668f846180a036dee
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-08-08 13:46:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+//you
+firstChar(s){
+	for(let i=0; i<s.length; i++){
+		 if(!s.contains(s.charAt(i)))
+			 return s.charAt(i);
+	}
+	return -1;
+}
+
+const text = prompt("Enter a string");
+alert(firstChar(text));
