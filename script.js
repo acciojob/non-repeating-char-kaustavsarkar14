89f10ccb2628b3350a4dbf6199dd25f7a1ed708a
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-08-08 13:48:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 //you
-firstChar(s){
+
+
+const text = prompt("Enter a string");
+alert(firstChar(text));
+
+const firstChar =(s)=>{
 	for(let i=0; i<s.length; i++){
 		 if(!s.contains(s.charAt(i)))
 			 return s.charAt(i);
 	}
 	return -1;
 }
-
-const text = prompt("Enter a string");
-alert(firstChar(text));
